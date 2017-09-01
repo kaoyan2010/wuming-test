a62904ffd9833196bffab9afe94cf8dd960d88a0
liuhu <liuhu@ultrapower.com.cn>
2017-09-01 20:28:55 +0800

    第一次修改

--- src/main/java/com/wuming/test/App.java
+++ src/main/java/com/wuming/test/App.java
@@ -4,10 +4,12 @@ package com.wuming.test;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public static void firstFun() {
+		System.out.println("第一个函数");
+	}
 }
